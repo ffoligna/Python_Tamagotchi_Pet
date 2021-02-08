4d38dfab5ff894382dcb2bd95fe713ec26bbb67c
Federico Foligna <ffoligna@users.noreply.github.com>
2021-02-08 05:14:02 -0300

    Delete SimuladorDeDado (6 caras).py

--- SimuladorDeDado (6 caras).py
+++
@@ -1,9 +0,0 @@
-import random
-
-pregunta = "Si desea tirar el dado, escriba 'S': "
-
-def respuesta():
-    return (input(pregunta))
-
-while respuesta() == "S":
-    print (random.choice(range(1,7)))
